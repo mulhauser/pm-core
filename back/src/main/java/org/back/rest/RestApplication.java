267449e7a0a436e1f2904df93f338e7aa9b45699
Flavien.M <mondyflavien@gmail.com>
2017-12-08 16:11:22 +0100

    fix rest service

--- back/src/main/java/org/back/rest/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package org.back.rest;
-
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("/rest")
-public class RestApplication extends Application {
-}
