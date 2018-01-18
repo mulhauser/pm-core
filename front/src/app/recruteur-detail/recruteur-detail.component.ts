6b2af1cf89bb89c3f6d1e8a4dd59c805804efc90
MagicKitty <blin.nicolas.etu@gmail.com>
2018-01-18 21:52:14 +0100

    recruteur + navbar + footer

---
+++ front/src/app/recruteur-detail/recruteur-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recruteur-detail',
+  templateUrl: './recruteur-detail.component.html',
+  styleUrls: ['./recruteur-detail.component.css']
+})
+export class RecruteurDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
