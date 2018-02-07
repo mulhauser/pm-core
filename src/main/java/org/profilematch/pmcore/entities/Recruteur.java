070a1f35b70f0ebaf237f7ac247ec1d2e8f8175a
Flavien.M <mondyflavien@gmail.com>
2018-02-07 13:07:26 +0100

    mise a jour recruteur

--- src/main/java/org/profilematch/pmcore/entities/Recruteur.java
+++ src/main/java/org/profilematch/pmcore/entities/Recruteur.java
@@ -1,29 +1,48 @@
 package org.profilematch.pmcore.entities;
 
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
-
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Collection;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
 @Entity
 @DiscriminatorValue("R")
 public class Recruteur extends Profil {
 
-    public Recruteur(){
-        super();
+    @OneToMany(mappedBy = "recruteur", fetch = FetchType.LAZY)
+    private Collection<Offre> offres;
+
+   /* @OneToMany(mappedBy = "id.recruteur")
+    //@Cascade({CascadeType.SAVE_UPDATE})
+    private Collection<DemandeAvis> demandeAvis;
+
+    public Collection<DemandeAvis> getDemandeAvis() {
+        return demandeAvis;
     }
 
-    public Recruteur(String nom, String prenom, String pays, String ville, String codePostal, String email, String telephone, Calendar dateNaissance, String titre, String apropos, String status) {
+    public void setDemandeAvis(Collection<DemandeAvis> demandeAvis) {
+        this.demandeAvis = demandeAvis;
+    }*/
+
+
+    public Recruteur() {
+    }
+
+
+    public Recruteur(String nom, String prenom, String pays, String ville, String codePostal, String email, String telephone, Calendar dateNaissance, String titre, String apropos,  String status) {
         super(nom, prenom, pays, ville, codePostal, email, telephone, dateNaissance, titre, apropos, status);
     }
 
-    public Recruteur(String lastName, String firstName, String email) {
-
-        super(lastName, firstName, email);
+    public Collection<Offre> getOffres() {
+        return offres;
     }
+    public void setOffres(Collection<Offre> offres) {
+        this.offres = offres;
+    }
+
+
+
 
 }
