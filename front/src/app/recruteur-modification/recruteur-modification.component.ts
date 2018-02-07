2c1f0b0c7560ae109841c8500b5248b083d1c265
wolkowicz <michelwolkowicz@yahoo.fr>
2018-02-07 15:13:04 +0100

    commit verif 1

--- front/src/app/recruteur-modification/recruteur-modification.component.ts
+++ front/src/app/recruteur-modification/recruteur-modification.component.ts
@@ -29,7 +29,7 @@ export class RecruteurModificationComponent implements OnInit {
 
   @Input('recruteurDetail')
   set recruteur (recruteurDetail: any){
-    this.candidatDetail = candidatDetail;
+    this.recruteurDetail = recruteurDetail;
   }
 
   get urlPhoto(): string {
@@ -43,101 +43,15 @@ export class RecruteurModificationComponent implements OnInit {
 
   update()  {
     // console.log('update ' + candidat.firstname);
-    this.candidatService.updateCandidat(this.candidatDetail)
+    this.recruteurService.updateRecruteur(this.recruteurDetail)
       .subscribe(data =>
         this.alertService.success('Modifications effectuées', true)
       );
 
     if (this._urlPhoto != null || this._urlPhoto.length !== 0) {
-      this.userService.updatePhoto(this.candidatDetail.email, this._urlPhoto).subscribe();
+      this.userService.updatePhoto(this.recruteurDetail.email, this._urlPhoto).subscribe();
     }
 
-  }
-  /*
-    cancel() {
-      this.alertService.clear();
-      this.router.navigate(['/home']);
-    }*/
-
-
-  showModalExperience() {
-    // set dialog status
-    this._dialogStatus = 'active';
-    // open modal
-    const dialogRef = this._experienceDialog.open(ModalAjoutExperienceComponent, {
-      size: 'lg',
-      keyboard: true,
-      backdrop: 'static'
-    });
-    dialogRef.result.then(
-      (result) => {
-        this._addExperience(result.value)
-          .subscribe(
-            (infoExperience: any) => {
-              this._infoExperience = infoExperience;
-            },
-            () => this._dialogStatus = 'inactive',
-            () => {this._dialogStatus = 'inactive'; window.location.reload();}
-          );
-      }, (reason) => {
-        this._dialogStatus = 'inactive';
-      }
-    );
-  }
-
-
-
-  showModalCompetence() {
-    // set dialog status
-    this._dialogStatus = 'active';
-    // open modal
-    const dialogRef = this._competenceDialog.open(ModalAjoutCompetenceComponent, {
-      size: 'lg',
-      keyboard: true,
-      backdrop: 'static'
-    });
-    dialogRef.result.then(
-      (result) => {
-        this._addCompetence(result.value)
-          .subscribe(
-            (infoCompetence: any) => {
-              this._infoCompetence = infoCompetence;
-            },
-            () => this._dialogStatus = 'inactive',
-            () => this._dialogStatus = 'inactive'
-          );
-      }, (reason) => {
-        this._dialogStatus = 'inactive';
-      }
-    );
-  }
-
-
-
-  private _addExperience (experience: any): Observable<any> {
-    return this.candidatService.addCandidatExperience(experience, this.candidatDetail.id)
-      .flatMap(_ => _);
-  }
-
-  private _addCompetence (competence: any): Observable<any> {
-    return this.candidatService.addCandidatCompetence(competence, this.candidatDetail.id, competence.idCompetence)
-      .flatMap(_ => _);
-  }
-
-
-
-
-
-
-
-
-
-
-  get dialogStatus(): string {
-    return this._dialogStatus;
-  }
-
-  updateExperiences(){
   }
 
 }
@@ -154,14 +68,4 @@ export class RecruteurModificationComponent implements OnInit {
 
 
 
-
-
-
-
-
-
-
-
-
-/////////////////////
 
